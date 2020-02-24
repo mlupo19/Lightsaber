e10e26224aa2e6ecae1452be2192d5a8f2c630bb
jponte1 <jpontelandolfo1@gmail.com>
2020-02-24 08:20:41 -0500

    hewwo

--- app/src/main/java/gov/unsc/lupo/lightsaber/MainActivity.java
+++ app/src/main/java/gov/unsc/lupo/lightsaber/MainActivity.java
@@ -2,17 +2,25 @@ package gov.unsc.lupo.lightsaber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     private boolean on = false;
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
 
     public void clickOn(View view) {
