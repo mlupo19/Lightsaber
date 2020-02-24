010adfbba8f67ea40b2200f0244253aa06194560
mlupo <mike.lupo19@gmail.com>
2020-02-24 08:16:34 -0500

    soudns

--- app/src/main/java/gov/unsc/lupo/lightsaber/MainActivity.java
+++ app/src/main/java/gov/unsc/lupo/lightsaber/MainActivity.java
@@ -2,17 +2,21 @@ package gov.unsc.lupo.lightsaber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     private boolean on = false;
+    private MediaPlayer onMp, offMp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onMp = MediaPlayer.create(this, R.raw.saber_on);
+        offMp = MediaPlayer.create(this, R.raw.saber_crash);
     }
 
     public void clickOn(View view) {
@@ -26,11 +30,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void playOnSound() {
-
+        onMp.start();
     }
 
     private void playOffSound() {
-
+        offMp.start();
     }
 
 }
